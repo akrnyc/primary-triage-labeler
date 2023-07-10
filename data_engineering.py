713ca9f38341f26dc7a058d9c65703f35eec8ba0
Alexandrea Ramnarine <alxramnarine@gmail.com>
2023-07-10 18:09:12 -0400

    init feature extraction

--- data_engineering.py
+++ data_engineering.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from preprocessing import doc_preprocess
 from sklearn.preprocessing import LabelBinarizer
+from transformers import pipeline
 
 data = pd.read_csv('minintdb.csv',
                    index_col = 'INC_KEY',
@@ -20,3 +21,10 @@ target = lb.transform(data['label'])
 
 #process features, X
 data['corpus'] = data['DCODEDESCR'].apply(doc_preprocess, trim='lemma')
+
+extractor = pipeline(model = "bart-base", 
+                     task = "feature-extraction",
+                     framework = "tf",
+                     ) #https://huggingface.co/facebook/bart-base
+
+data['features'] = data['corpus'].apply(extractor)
